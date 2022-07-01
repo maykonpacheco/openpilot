018de5edaff63cc042b7ae73ba9ce2ea02871fa5
AlexandreSato <sato_alexandre@hotmail.com>
2022-07-01 10:22:54 -0300

    trying remote lock doors

---
+++ doors.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+import time
+from panda import panda
+
+lockCommand = {0x40, 0x05, 0x30, 0x11, 0x00, 0x40, 0x00, 0x00}
+unlockCommand = {0x40, 0x05, 0x30, 0x11, 0x00, 0x80, 0x00, 0x00}
+p = Panda()
+
+def main():
+  p.set_safety_mode(Panda.SAFETY_ALLOUTPUT)
+
+  # args
+  if len(sys.argv) != 2:
+    sys.exit('usage$: doors.py --lock or --unlock')
+
+  if sys.argv[1]  == '--lock' or sys.argv[1]  == '-l':
+    print(f'command: {lockCommand}')
+    p.can_send(0x750, lockCommand, 0) 
+
+  if sys.argv[1] == '--unlock' or sys.argv[1] == '-u':
+    print(f'unlock command: {unlockCommand}')
+    p.can_send(0x750, unlockCommand, 0) 
+
+  time.sleep(0.2)
+  p.set_safety_mode(Panda.SAFETY_TOYOTA)
+  p.send_heartbeat()
+  print('\n\n\nrelay ON again\nkthxbay\n')
+
+main()
