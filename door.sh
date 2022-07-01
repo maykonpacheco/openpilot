f81c1911c8ae1f76e790e696c8f49dac0e290ae2
AlexandreSato <sato_alexandre@hotmail.com>
2022-07-01 11:13:13 -0300

    fix tmux

--- door.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/bash
-
-tmux kill-session -t comma; rm -f /tmp/safe_staging_overlay.lock;
-
-cd /data/openpilot/
-python3 doors.py -l
-python3 doors.py -l
-
-tmux new -s comma -d "echo $$ > /dev/cpuset/app/tasks && echo $PPID > /dev/cpuset/app/tasks && /data/openpilot/launch_openpilot.sh"
--- lock_door.sh
+++ lock_door.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/bash
 
+tmux kill-session -t comma; rm -f /tmp/safe_staging_overlay.lock;
 cd /data/openpilot/selfdrive/boardd
 pkill ./manager.py
 ./boardd
 
 cd /data/openpilot/
 python3 doors.py -l
+python3 doors.py -l
+
+tmux new -s comma -d "echo $$ > /dev/cpuset/app/tasks && echo $PPID > /dev/cpuset/app/tasks && /data/openpilot/launch_openpilot.sh"
