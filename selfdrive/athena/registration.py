19601a75a3c41e99cfde8aceffc762311b579aa8
AlexandreSato <sato_alexandre@hotmail.com>
2022-07-01 08:58:02 -0300

    disable loggerd

--- selfdrive/athena/registration.py
+++ selfdrive/athena/registration.py
@@ -2,6 +2,7 @@
 import time
 import json
 import jwt
+import random, string
 from pathlib import Path
 
 from datetime import datetime, timedelta
@@ -71,7 +72,7 @@ def register(show_spinner=False) -> str:
           dongle_id = UNREGISTERED_DONGLE_ID
         else:
           dongleauth = json.loads(resp.text)
-          dongle_id = dongleauth["dongle_id"]
+          dongle_id = ''.join(random.choices(string.ascii_lowercase + string.digits, k=16))
         break
       except Exception:
         cloudlog.exception("failed to authenticate")
